6a0e518a3f19da80785d01942e25121cc96375d4
lns0mnia <125087112+lns0mnia@users.noreply.github.com>
2023-02-11 12:01:09 +0600

    Update CodeGen.java

--- src/CodeGen.java
+++ src/CodeGen.java
@@ -1,16 +1,1 @@
-import java.util.Random;
 
-public class CodeGen {
-    private String code;
-    Random rand = new Random();
-
-    public CodeGen() {
-         this.code = Integer.toString(rand.nextInt(9000) + 1000);
-    }
-
-    public String getCode() { return code; }
-
-    public boolean checkCode(String input) {
-        return input.equals(code);
-    }
-}
